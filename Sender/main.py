fe15cf0b73f359fd8cf34611bd9207c6b13e5dd2
sarfrazhussayn <35394114+sarfrazhussayn@users.noreply.github.com>
2019-11-20 12:22:36 +0500

    Initial Commits
    Working Sender and Split

---
+++ Sender/main.py
@@ -0,0 +1,57 @@
+import os
+import urllib.request
+from app import app
+from flask import Flask, flash, request, redirect, render_template, jsonify
+from werkzeug.utils import secure_filename
+
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif','mp4'])
+
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+	
+@app.route('/')
+def upload_form():
+	return render_template('upload.html')
+
+@app.route('/', methods=['POST'])
+def upload_file():
+	if request.method == 'POST':
+        # check if the post request has the files part
+		if 'files[]' not in request.files:
+			flash('No file part')
+			return redirect(request.url)
+		files = request.files.getlist('files[]')
+		for file in files:
+			if file and allowed_file(file.filename):
+				filename = secure_filename(file.filename)
+				file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		flash('File(s) successfully uploaded')
+		return redirect('/')
+
+@app.route('/file-upload', methods=['POST'])
+def send_file():
+	# check if the post request has the file part
+	if 'file' not in request.files:
+		resp = jsonify({'message' : 'No file part in the request'})
+		resp.status_code = 400
+		return resp
+	file = request.files['file']
+	if file.filename == '':
+		resp = jsonify({'message' : 'No file selected for uploading'})
+		resp.status_code = 400
+		return resp
+	if file and allowed_file(file.filename):
+		filename = secure_filename(file.filename)
+		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		resp = jsonify({'message' : 'File successfully uploaded'})
+		resp.status_code = 201
+		return resp
+	else:
+		resp = jsonify({'message' : 'Allowed file types are txt, pdf, png, jpg, jpeg, gif, mp4'})
+		resp.status_code = 400
+		return resp
+
+
+
+if __name__ == "__main__":
+    app.run()
